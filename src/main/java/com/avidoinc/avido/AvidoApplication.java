173918c604cccc543cee08b8dc9e5b9c35eac697
ankis <an.kis2011@yandex.ru>
2026-09-20 22:48:01 +0300

    Init

---
+++ src/main/java/com/avidoinc/avido/AvidoApplication.java
@@ -0,0 +1,13 @@
+package com.avidoinc.avido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvidoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avidoinc/avido/AvidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avidoinc.avido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
